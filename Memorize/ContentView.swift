3005f40e96ded97cbc76b4aacbf0d1c8863060be
Frank Jin <frank.jin@berkeley.edu>
2024-01-18 08:26:44 -0800

    Lecture 2: 27:57

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hi")
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
+        
+    }
+}
+
+struct CardView: View {
+    //@State is only used to define temp state. Makes a pointer to memory so var is not actually changed, only the value at the memory.
+    @State var isFaceUp = false
+    
+    var body: some View {
+        //Trailing closue syntax for content (only if last arg is a function. For example Zstack(allignment:..., ..., content: {}) -> Zstack(allignment: default){} -> Zstack {}
+        
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
